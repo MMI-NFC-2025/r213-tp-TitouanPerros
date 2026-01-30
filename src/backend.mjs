2afe86f867e1f18de03e29255cd0e00b074c1e8a
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2026-01-30 09:54:13 +0100

    commit 2

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const db = new PocketBase("http://127.0.0.1:8090");
+const db = new PocketBase('http://127.0.0.1:8090');
 
 export async function getOffres() {
     try {
@@ -12,4 +12,8 @@ export async function getOffres() {
         console.log('Une erreur est survenue en lisant la liste des maisons', error);
         return [];
     }
+}
+
+export async function getImageUrl(record, recordImage) {
+    return db.files.getUrl(record, recordImage);
 }
