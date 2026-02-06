472adf3e1acdaf2b11c442bf3132e83ab58d69e0
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2026-02-06 08:47:53 +0100

    création de la partie voir plus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config' ;
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -11,5 +11,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: vercel()
+  adapter: vercel(),
+  output: 'server',
+
 });
--- src/backend.mjs
+++ src/backend.mjs
@@ -17,3 +17,12 @@ export async function getOffres() {
 export async function getImageUrl(record, recordImage) {
     return db.files.getUrl(record, recordImage);
 }
+export async function getOffre(id) {
+    try {
+        const data = await db.collection('maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
+    }
+}
